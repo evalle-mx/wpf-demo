55e1fecbc4f1b5fd50872847ffaeac1dce71b6af
evalle <evalle-mx@gmail.com>
2020-12-01 20:47:05 -0500

    V.2 {WPF Controls: Learn basic WPF controls in C#} + ComboBox (Usando DataTemplate == Dto) + Image + MediaElement + StackPanel + WrapPanel + ScrollViewer   + StackPanel dentro del ComboBox para mostrar imagenes en el combo

--- ReadMe.txt
+++ ReadMe.txt
@@ -4,3 +4,22 @@
 ###  https://www.youtube.com/watch?v=gSfMNjWNoX0		###
 ###  &list=PLLWMQd6PeGY3QEHCmCWaUKNhmFFdIDxE8			###
 ###########################################################
+?? cd /c/Users/netto/Documents/ProgrammingC#/WPFDemo ??
+
+
+V.1. {Intro to WPF: Learn the basics and best practices of WPF for C#}
+* Bases de WPF
++ Grid distribution (Column & Row definicion)
++ TextBlock
++ Button
++ Mensaje en pantalla
+
+
+V.2 {WPF Controls: Learn basic WPF controls in C#}
++ ComboBox (Usando DataTemplate == Dto)
++ Image
++ MediaElement
++ StackPanel
++ WrapPanel
++ ScrollViewer
+  + StackPanel dentro del ComboBox para mostrar imagenes en el combo
--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -20,14 +20,40 @@ namespace WPFDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Person> people = new List<Person>();
+        
         public MainWindow()
         {
+            string imgPath = "C:\\Users\\netto\\Pictures\\demo\\"; //No funciona con Path relativo
             InitializeComponent();
+
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey", Icon= imgPath+"Icons\\Luma-Red.png" });
+            people.Add(new Person { FirstName = "Violeta", LastName = "Romero", Icon = imgPath + "Icons\\Luma-Yellow.png" });
+            people.Add(new Person { FirstName = "Breneli", LastName = "Galvez", Icon = imgPath + "Icons\\Luma-Green.png" });
+
+
+            myComboBox.ItemsSource = people;
         }
 
         private void submitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"Hola {firstNameText.Text}");
+        }
+    }
+
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Icon { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{ FirstName } { LastName } ";
+            }
         }
     }
 }
